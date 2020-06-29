a06d2a3680d9694e75904ac31430c4cf2653da40
quartet <quartet@Mac-mini-of-quartet.local>
2020-06-29 20:12:43 +0900

    edit

---
+++ build/precache-manifest.5a08dc9562f14c7b37f6ae585deeb3b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e34a5f0261eb7295ae6e0f2cab9b96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9631bd5d95c91a9f88c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "db26947038e7ad9a62d3",
+    "url": "/static/js/2.a5bcdf1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5bcdf1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9631bd5d95c91a9f88c",
+    "url": "/static/js/main.946f5e04.chunk.js"
+  },
+  {
+    "revision": "0fab776da41d98c64114",
+    "url": "/static/js/runtime-main.38043156.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
