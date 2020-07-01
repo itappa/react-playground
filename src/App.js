629e3fa81e6bbe5c0fc0a28fb7489fcd612a888c
itappa <t.quartet@gmail.com>
2020-07-02 06:54:23 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-				<p id="timer" class="circle">25:00<br>
+				<p id="timer" class="circle">25:00<br />
           <a href="">Start</a>
         </p>
       </header>
-      <p class="circle timer">5:00<br>
+      <p class="circle timer">5:00<br />
         <a href="">Break</a>
       </p>
     </div>
