90e14bc3db6def59b368b07cad47c98c8a016995
itappa <t.quartet@gmail.com>
2020-07-01 20:30:56 +0900

    edit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
       <header className="App-header">
 				<p id="timer" class="circle">25:00</p>
       </header>
+      <p class="circle timer">5:00</p>
+      <a class="circle button">5:00</p>
     </div>
   );
 }
+
 
 export default App;
